6982069c0edac953d4bd5e815136c831eec34219
rtberih <radwan.berih1@gmail.com>
2023-01-13 01:44:19 -0500

    function for findkey

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const hotels = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(hotels, x => x.stars === 2), "noma");
+assertEqual(findKey(hotels, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(hotels, x => x.stars === 5), undefined);
